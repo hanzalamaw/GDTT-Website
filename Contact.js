504c0d8ab4038fa121d47fb05834377d30e17840
Hanzala M A Wahab <hanzalamawahab@gmail.com>
2025-02-28 23:12:00 +0500

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,83 @@
+// HAMBURGER MENU
+
+const menuToggle = document.getElementById('menuToggle');
+const navOptions = document.getElementById('navOptions');
+
+menuToggle.addEventListener('click', () => {
+    navOptions.classList.toggle('show');
+    menuToggle.classList.toggle('open');
+});
+
+document.querySelectorAll('.navItem').forEach(item => {
+    item.addEventListener('click', () => {
+        navOptions.classList.remove('show');
+        menuToggle.classList.remove('open'); 
+    });
+});
+
+//SEND DATA TO SHEETS
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    document.getElementById("done").style.display = "block";
+    
+    event.preventDefault(); 
+    
+    let data = {
+        from: "Contact Us Page",
+        package: document.getElementById("message").value,
+        name: document.getElementById("name").value,
+        contact: document.getElementById("contact").value,
+        email: document.getElementById("email").value,
+        persons: document.getElementById("trip").value
+    };
+    
+    document.getElementById("name").value = "";
+    document.getElementById("contact").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("trip").value = "";
+    document.getElementById("message").value = "";
+    
+    fetch("https://script.google.com/macros/s/AKfycbxMQ42vYRcCYv2kuIqYDdSN9J9sfZLcQwsVsc_bidfkO1ZMgcyJIpqTxakUqFDFURgeVg/exec", {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(() => {document.getElementById("done").style.display = "block";
+                document.getElementById("fail").style.display = "none"})
+    .catch(error => {
+            document.getElementById("done").style.display = "none";
+            document.getElementById("fail").style.display = "block";
+        });
+    });
+
+// SEND EMAIL SUBSCRIPTIONS 
+
+document.getElementById("news").addEventListener("submit", function(event) {
+    
+    event.preventDefault(); 
+    
+    let data = {
+        from: "NewsLetter Subscription",
+        package: "-",
+        name: "-",
+        contact: "-",
+        email: document.getElementById("news-email").value,
+        persons: "-"
+    };
+    
+    document.getElementById("news-email").value = "";
+    
+    fetch("https://script.google.com/macros/s/AKfycbxMQ42vYRcCYv2kuIqYDdSN9J9sfZLcQwsVsc_bidfkO1ZMgcyJIpqTxakUqFDFURgeVg/exec", {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(() => {console.log("Done!");})
+    .catch(error => {console.log("Done!");});
+});
