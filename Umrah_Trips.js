c4da0ace1ae21b85e2652bffb407cd38d4cd8e96
Hanzala M A Wahab <hanzalamawahab@gmail.com>
2025-03-02 19:21:24 +0500

    Add files via upload

--- Umrah_Trips.js
+++ Umrah_Trips.js
@@ -1,3 +1,26 @@
+// fade in animation
+
+document.addEventListener("DOMContentLoaded", function () {
+const sections = document.querySelectorAll(".fade-in-section");
+
+const observer = new IntersectionObserver(
+    (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+                observer.unobserve(entry.target); // Unobserve after first reveal
+            }
+        });
+    },
+    { threshold: 0.2 } // Adjust threshold for earlier/later triggering
+);
+
+sections.forEach(section => {
+    observer.observe(section);
+});
+});
+    
+    
 // HAMBURGER MENU
 
 const menuToggle = document.getElementById('menuToggle');
