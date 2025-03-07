30a74e0ccf546c2a27387f062cdb584aefeb82cb
Hanzala M A Wahab <hanzalamawahab@gmail.com>
2025-03-07 21:49:37 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -38,6 +38,65 @@ document.querySelectorAll('.navItem').forEach(item => {
     });
 });
 
+// FAQs
+
+function display(plus, answer, plus_selected) {
+    document.getElementById(plus).style.display = "none";
+    document.getElementById(answer).style.display = "block";
+    document.getElementById(plus_selected).style.display = "block";
+
+    let answerElement = document.getElementById(answer);
+    answerElement.classList.add('visible');
+
+}
+
+function hide(plus, answer, plus_selected) {
+    document.getElementById(plus).style.display = "block";
+    document.getElementById(plus_selected).style.display = "none";
+
+    let answerElement = document.getElementById(answer);
+    answerElement.classList.remove('visible');
+}
+
+//SEND DATA TO SHEETS
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    document.getElementById("done").style.display = "block";
+    
+    event.preventDefault(); 
+    
+    let data = {
+        from: "Contact Us Page",
+        package: document.getElementById("message").value,
+        name: document.getElementById("name").value,
+        contact: document.getElementById("contact").value,
+        email: document.getElementById("email").value,
+        persons: document.getElementById("trip").value
+    };
+    
+    document.getElementById("name").value = "";
+    document.getElementById("contact").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("trip").value = "";
+    document.getElementById("message").value = "";
+    
+    fetch("https://script.google.com/macros/s/AKfycbxMQ42vYRcCYv2kuIqYDdSN9J9sfZLcQwsVsc_bidfkO1ZMgcyJIpqTxakUqFDFURgeVg/exec", {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(() => {document.getElementById("done").style.display = "block";
+                document.getElementById("fail").style.display = "none"})
+    .catch(error => {
+            document.getElementById("done").style.display = "none";
+            document.getElementById("fail").style.display = "block";
+        });
+    });
+
+
 // SEND EMAIL SUBSCRIPTIONS 
 
 document.getElementById("news").addEventListener("submit", function(event) {
